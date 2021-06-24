83e0c435678ca2d00e57e2161867b50494f35e7d
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-06-24 17:40:39 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
-const cache = { "": 120 } as { [key: string]: number };
+const INITIAL = 128;
+const cache = { "": INITIAL } as { [key: string]: number };
 // @ts-ignore
 window.adjust = () => {
   Array.from(document.getElementsByClassName("fusen")).forEach((_x) => {
     const x = _x as HTMLDivElement;
     console.log(x.innerText, x.scrollHeight, cache[x.innerText]);
     if (cache[x.innerText] === undefined) {
-      cache[x.innerText] = 120;
+      cache[x.innerText] = INITIAL;
     }
+    // let left = 1;
+    // let right = INITIAL;
+    // let current = INITIAL;
+    // let step = INITIAL / 2;
+    // while (true) {
+    //   if (x.scrollHeight > 100) {
+    //     cache[x.innerText] -= step;
+
+    // }
     while (true) {
       if (x.scrollHeight > 100) {
         cache[x.innerText] -= 1;
@@ -21,44 +31,40 @@ window.adjust = () => {
       }
       break;
     }
+    if (x.style.fontSize === "0px") {
+      x.style.fontSize = "1px";
+    }
   });
 };
+
+type Props = {
+  text: string;
+};
+
+const Fusen: React.FC<Props> = ({ children, text }) => {
+  const [fontSize, setFontSize] = useState(INITIAL);
+  const ref = useRef(null);
+
+  return (
+    <div className="fusen" ref={ref} draggable={true}>
+      <div>{text}</div>
+    </div>
+  );
+};
+
 function App() {
+  let a = 1;
+  let b = 1;
+  const texts = [];
+  for (let i = 0; i < 10; i++) {
+    [a, b] = [b, a + b];
+    texts.push("あ".repeat(a));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="fusen" id="test">
-          a
-        </div>
-        <p></p>
-        <div className="fusen" id="test">
-          aa
-        </div>
-        <p></p>
-        <div className="fusen" id="test">
-          aaa
-        </div>
-        <p></p>
-        <div className="fusen" id="test">
-          あ
-        </div>
-        <p></p>
-        <div className="fusen" id="test">
-          ああ
-        </div>
-        <p></p>
-        <div className="fusen" id="test">
-          <span>あああ</span>
-        </div>
-        <p></p>
-        <div className="fusen" id="test">
-          aaaaaaaaaaaaaaaaaaaaa
-        </div>
-        <p></p>
-        <div className="fusen" id="test">
-          aaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaa
-        </div>
-      </header>
+      {texts.map((text) => (
+        <Fusen text={text} />
+      ))}
     </div>
   );
 }
