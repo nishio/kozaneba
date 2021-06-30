487e57626975a6b87a61fe6b92fd2afe4d01b813
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-06-30 16:53:06 +0900

    add test

---
+++ cypress/integration/1-getting-started/many_fusen.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe('adjust font size', () => {
+  beforeEach(() => {
+    cy.visit('/')
+    const fusens = []
+    for (let x = -10; x < 11; x++){
+      for (let y = -10; y < 11; y++){
+        fusens.push({text:`${x},${y}`, x: x*200, y: y*200})
+      }
+    }
+    cy.window().its('movidea').then(movidea => {
+      setTimeout(() => {
+        movidea.setGlobal({ fusens });        
+      })
+    });
+  })
+
+  it('position', () => {
+
+  })
+})
---
+++ src/testFixture_ManyFusen.tsx
@@ -0,0 +1,11 @@
+import { setGlobal } from "reactn";
+
+const testFixture_ManyFusen = () => {
+  const fusens = [];
+  for (let x = -10; x < 11; x++) {
+    for (let y = -10; y < 11; y++) {
+      fusens.push({ text: `${x},${y}`, x: x * 200, y: y * 200 });
+    }
+  }
+  setGlobal({ fusens });
+};
