0b8ef4edfe134ffaab804ea2b31be8bf61afda3b
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-07-29 17:52:47 +0900

    add firebase

---
+++ src/Cloud/FirestoreIO.ts
@@ -0,0 +1,53 @@
+// Initialize Firebase
+import firebase from "firebase/app";
+import "firebase/firestore";
+import { State } from "reactn/default";
+
+const config = {
+  apiKey: "AIzaSyB0wAxxeLeHr4udunpln5jCYpGpFGn7D00",
+  authDomain: "regroup-d4932.firebaseapp.com",
+  projectId: "regroup-d4932",
+};
+
+firebase.initializeApp(config);
+
+const db = firebase.firestore();
+
+export default db;
+
+// types
+export type DocSnap =
+  firebase.firestore.DocumentSnapshot<firebase.firestore.DocumentData>;
+
+export type DocRef =
+  firebase.firestore.DocumentReference<firebase.firestore.DocumentData>;
+
+export type DocData = firebase.firestore.DocumentData;
+
+export const load_from_server = (data: DocData): void => {};
+export const save_to_server = (state: State): void => {};
+
+export const state_to_docdate = (state: State): DocData => {
+  return new DocDataV3();
+};
+export const docdate_to_state = (data: DocData): Partial<State> => {
+  return {};
+};
+
+class DocDataV3 implements DocData {}
+
+export const create_new_map = () => {
+  const docdata = new DocDataV3();
+  _save(docdata).then((docRef: DocRef) => {});
+};
+export const load_map = () => {};
+
+const add_map = (doc: DocData): Promise<DocRef> => {
+  return db.collection("map").add(doc);
+};
+
+const add_key = (docRef: DocRef): Promise<DocRef> => {
+  return db.collection("key_to_map").add({ mapname: docRef.id });
+};
+
+const _save = (doc: DocData) => add_map(doc).then(add_key);
