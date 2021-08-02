f8e8555e68781b80ca89e2b97341758ca10eb7ce
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-08-02 17:08:35 +0900

    parse hash

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -55,19 +55,20 @@ function App() {
     window.addEventListener("wheel", onWheel, { passive: false });
   }, []);
 
-  const hash = window.location.hash;
-  if (hash === "#blank") {
+  if (document.location.hash === "") {
+    return <Tutorial />; // tutorial for first visiter
+  }
+
+  const hash = new URLSearchParams(window.location.hash.substring(1));
+  if (hash.has("blank")) {
     // for test
     return <Blank />;
   }
-  if (hash === "#tinysample") {
+  if (hash.has("tinysample")) {
     // for test
     return <TinySample />;
   }
-  if (hash === "") {
-    return <Tutorial />; // tutorial for first visiter
-  }
-  // TODO: parse hash
+
   return <></>;
 }
 
