5cab923d51a01dc457781deb9d457db04ea90bb7
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-08-17 00:23:26 +0900

    change color

--- src/Group/GroupDiv.tsx
+++ src/Group/GroupDiv.tsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import { TITLE_HEIGHT, BORDER } from "../dimension/get_bounding_box";
-import { OPACITY } from "../Kozane/kozane_constants";
-import { GROUP_BORDER_COLOR } from "./group_constants";
+import { GROUP_BACKGROUND_COLOR, GROUP_BORDER_COLOR } from "./group_constants";
 
 export const GroupDiv = styled.div`
   border: ${BORDER}px solid ${GROUP_BORDER_COLOR};
@@ -10,10 +9,9 @@ export const GroupDiv = styled.div`
 
 export const GroupBack = styled.div`
   position: absolute;
-  background: #eee;
+  background: ${GROUP_BACKGROUND_COLOR};
   width: 100%;
   height: 100%;
-  opacity: ${OPACITY};
 `;
 
 export const GroupTitle = styled.span`
--- src/Group/group_constants.tsx
+++ src/Group/group_constants.tsx
@@ -1,3 +1,4 @@
-export const GROUP_BORDER_COLOR = "#eee";
+export const GROUP_BORDER_COLOR = "rgba(255, 255, 255, .5)";
 export const CLOSED_GROUP_BORDER_COLOR = "#ddd";
 export const GROUP_HIGHLIGHTED_BORDER_COLOR = "#88f";
+export const GROUP_BACKGROUND_COLOR = "rgba(220, 220, 220, .5)";
