2e1fb3e844ff269de1a142b168d6b96daf0d8c06
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-08-18 00:58:16 +0900

    zoom around mouse pointer

--- cypress/integration/kozaneba/ready_one_kozane.ts
+++ cypress/integration/kozaneba/ready_one_kozane.ts
@@ -15,4 +15,17 @@ describe("ready one kozane", () => {
     cy.testid("ba").trigger("mouseup", 400, 400);
   });
   it("do nothing", () => {});
+  it("zoom center", () => {
+    cy.testid("ba").trigger("mousemove");
+    cy.testid("ba").trigger("wheel", { ctrlKey: true, deltaY: 100 });
+    cy.testid("ba").trigger("wheel", { ctrlKey: true, deltaY: -100 });
+
+    cy.testid("ba").trigger("mousemove", 100, 100);
+    cy.testid("ba").trigger("wheel", { ctrlKey: true, deltaY: 100 });
+    cy.testid("1").should(
+      "hasPosition",
+      [130.90187072753906, 136.42005920410156]
+    );
+    // cy.testid("ba").trigger("wheel", { ctrlKey: true, deltaY: -100 });
+  });
 });
--- src/Event/onWheel.tsx
+++ src/Event/onWheel.tsx
@@ -1,10 +1,37 @@
+import { add_v2, mul_v2, sub_v2, V2 } from "../dimension/V2";
+import { screen_to_world, TWorldCoord } from "../dimension/world_to_screen";
 import { updateGlobal } from "../Global/updateGlobal";
+import { get_last_mouse_position } from "./onCanvasMouseMove";
+
+export function zoomAroundMousePointer(
+  initZoom: number,
+  initCenter: TWorldCoord,
+  zoomCenter: TWorldCoord,
+  scale: number
+): [number, V2] {
+  const v = sub_v2(initCenter, zoomCenter);
+  let newZoom = initZoom * scale;
+  let newCenter = add_v2(initCenter, mul_v2((1 - scale) / scale, v));
+  return [newZoom, newCenter];
+}
+
+const deltaY_to_scale = (v: number) => {
+  return Math.exp(-v / 100);
+};
 
 export const onWheel = (e: WheelEvent) => {
   e.preventDefault();
   if (e.ctrlKey) {
     updateGlobal((g) => {
-      g.scale *= Math.exp(-e.deltaY / 100);
+      const [newZoom, newCenter] = zoomAroundMousePointer(
+        g.scale,
+        [-g.trans_x, -g.trans_y] as TWorldCoord,
+        screen_to_world(get_last_mouse_position()),
+        deltaY_to_scale(e.deltaY)
+      );
+      g.scale = newZoom;
+      g.trans_x = -newCenter[0];
+      g.trans_y = -newCenter[1];
     });
   } else if (e.shiftKey) {
     updateGlobal((g) => {
