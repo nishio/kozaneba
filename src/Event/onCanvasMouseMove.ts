250e6bd9fc242209887ceb99c694ea7205bee3f7
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-08-18 00:19:31 +0900

    record last mouse position

--- src/Event/onCanvasMouseMove.ts
+++ src/Event/onCanvasMouseMove.ts
@@ -1,16 +1,24 @@
 import React from "react";
 import { getGlobal } from "reactn";
+import { TScreenCoord } from "../dimension/world_to_screen";
 import { updateGlobal } from "../Global/updateGlobal";
 import { move_target } from "./fast_drag_manager";
+import { get_client_pos } from "./get_client_pos";
+
+let last_mouse_position = [0, 0] as TScreenCoord;
+export const get_last_mouse_position = () => {
+  return last_mouse_position;
+};
 
 export const onCanvasMouseMove = (
   event: React.MouseEvent<HTMLDivElement, MouseEvent>
 ) => {
+  last_mouse_position = get_client_pos(event);
   const g = getGlobal();
   if (g.mouseState === "selecting") {
     updateGlobal((g) => {
-      g.selectionRange.width = event.pageX - g.selectionRange.left;
-      g.selectionRange.height = event.pageY - g.selectionRange.top;
+      g.selectionRange.width = last_mouse_position[0] - g.selectionRange.left;
+      g.selectionRange.height = last_mouse_position[1] - g.selectionRange.top;
     });
   } else if (g.drag_target !== "") {
     // updateGlobal((g) => {
