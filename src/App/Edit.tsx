eb6bbd5cd2f78e0331b4a283d00340fc2b818f2c
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-08-19 14:30:59 +0900

    show loading

--- src/App/Edit.tsx
+++ src/App/Edit.tsx
@@ -1,11 +1,16 @@
 import { useEffect } from "react";
-import { Blank } from "./Blank";
 import { updateGlobal } from "../Global/updateGlobal";
+import { KozaneItem } from "../Kozane/KozaneItem";
+import { Blank } from "./Blank";
 
 export const Edit: React.FC<{ ba: string }> = ({ ba }) => {
   useEffect(() => {
     console.log("loading", ba);
     updateGlobal((g) => {
+      const loading = new KozaneItem();
+      loading.text = "loading...";
+      g.itemStore[loading.id] = loading;
+      g.drawOrder = [loading.id];
       g.statusBar.type = "downloading";
       g.cloud_ba = ba;
     });
