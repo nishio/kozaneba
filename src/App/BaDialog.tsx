1bd6a7126331477daeb1395a5cc96b0de931c0bf
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-08-27 19:41:39 +0900

    if user can not write, do not show form to edit

--- src/App/BaDialog.tsx
+++ src/App/BaDialog.tsx
@@ -11,13 +11,13 @@ import { useState } from "react";
 import { getGlobal, setGlobal, useGlobal } from "reactn";
 import { close_menu_and_dialog } from "../AppBar/close_menu";
 import { mark_local_changed } from "../Cloud/mark_local_changed";
+import { can_write } from "./can_write";
 
 export type Ba = { title: string; id: string; last_updated: number };
 
 export const BaDialog = () => {
   const [dialog] = useGlobal("dialog");
   const [anyone_writable] = useGlobal("anyone_writable");
-  const [copy_done, set_copy_done] = useState("");
   const open = dialog === "Ba";
   const mode = anyone_writable ? "edit" : "view";
 
@@ -25,30 +25,6 @@ export const BaDialog = () => {
     close_menu_and_dialog();
   };
 
-  const onChange = (
-    e: React.ChangeEvent<{
-      name?: string | undefined;
-      value: unknown;
-    }>
-  ) => {
-    const value = e.target.value;
-    const anyone_writable = value === "edit";
-    console.log(value, anyone_writable);
-    setGlobal({ anyone_writable });
-    mark_local_changed();
-  };
-  const g = getGlobal();
-  const share_url = `https://kozaneba.netlify.app/#${mode}=${g.cloud_ba}`;
-  const onCopy = () => {
-    navigator.clipboard
-      .writeText(share_url)
-      .then(() => {
-        set_copy_done("copy OK");
-      })
-      .catch(() => {
-        set_copy_done("copy ERROR");
-      });
-  };
   return (
     <Dialog
       open={open}
@@ -60,17 +36,7 @@ export const BaDialog = () => {
       <DialogTitle id="form-dialog-title">Ba</DialogTitle>
       <DialogContent style={{ padding: "0px 24px" }}>
         <Title />
-        <p>
-          URL: <button onClick={onCopy}>copy{copy_done}</button>
-          <input type="text" value={share_url} style={{ width: "30em" }} />
-        </p>
-        <p>
-          Anyone with the link can{" "}
-          <Select value={mode} onChange={onChange}>
-            <MenuItem value="edit">edit</MenuItem>
-            <MenuItem value="view">view</MenuItem>
-          </Select>
-        </p>
+        <Share mode={mode} />
       </DialogContent>
       <DialogActions>
         <Button color="primary" onClick={onClose}>
@@ -90,9 +56,56 @@ const Title = () => {
       mark_local_changed();
     }
   };
+  const button = can_write() ? <button onClick={onClick}>change</button> : null;
   return (
     <p>
-      Title: {title} <button onClick={onClick}>change</button>
+      Title: {title} {button}
     </p>
+  );
+};
+
+const Share: React.FC<{ mode: string }> = ({ mode }) => {
+  const [copy_done, set_copy_done] = useState("");
+  if (!can_write()) return null;
+
+  const g = getGlobal();
+  const share_url = `https://kozaneba.netlify.app/#${mode}=${g.cloud_ba}`;
+  const onChange = (
+    e: React.ChangeEvent<{
+      name?: string | undefined;
+      value: unknown;
+    }>
+  ) => {
+    const value = e.target.value;
+    const anyone_writable = value === "edit";
+    setGlobal({ anyone_writable });
+    mark_local_changed();
+  };
+
+  const onCopy = () => {
+    navigator.clipboard
+      .writeText(share_url)
+      .then(() => {
+        set_copy_done(" OK");
+      })
+      .catch(() => {
+        set_copy_done(" ERROR");
+      });
+  };
+
+  return (
+    <div>
+      <p>
+        URL: <button onClick={onCopy}>copy{copy_done}</button>
+        <input type="text" value={share_url} style={{ width: "30em" }} />
+      </p>
+      <p>
+        Anyone with the link can{" "}
+        <Select value={mode} onChange={onChange}>
+          <MenuItem value="edit">edit</MenuItem>
+          <MenuItem value="view">view</MenuItem>
+        </Select>
+      </p>
+    </div>
   );
 };
