1632b45065de47f826836785d29ef8cfde25c64b
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-08-28 13:38:06 +0900

    fix

--- src/App/LocalChangeWatcher.tsx
+++ src/App/LocalChangeWatcher.tsx
@@ -24,5 +24,6 @@ export const LocalChangeWatcher = () => {
 export const is_user_in_writers = () => {
   const g = getGlobal();
   if (g.user === null) return false;
+  if (g.writers === undefined) return false;
   return g.writers.includes(g.user.uid);
 };
