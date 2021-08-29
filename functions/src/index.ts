7e3befe803241fcba5ad48563fa4a83ed62ed6c5
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-08-30 02:16:10 +0900

    API to fetch Scrapbox API

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,15 +9,24 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
 
-export const get_scrapbox_page = functions.https.onRequest(
-  (request, response) => {
-    const body = JSON.parse(request.body);
-    const url = body.url;
-    const api_url = url.replace("scrapbox.io/", "scrapbox.io/api/pages/");
-    fetch(api_url).then((req) => {
-      req.text().then((text) => {
-        response.send(text);
-      });
-    });
+export const get_scrapbox_page = functions.https.onRequest((req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+
+  if (req.method === "OPTIONS") {
+    // Send response to OPTIONS requests
+    res.set("Access-Control-Allow-Methods", "GET");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Max-Age", "3600");
+    res.status(204).send("");
+    return;
   }
-);
+
+  const body = JSON.parse(req.body);
+  const url = body.url;
+  const api_url = url.replace("scrapbox.io/", "scrapbox.io/api/pages/");
+  fetch(api_url).then((req) => {
+    req.text().then((text) => {
+      res.send(text);
+    });
+  });
+});
