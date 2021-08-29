a56278d36c54f7d8c7a7740505f50451f16be3f4
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-08-30 02:30:46 +0900

    fix URL

--- src/API/KozanebaAPI.ts
+++ src/API/KozanebaAPI.ts
@@ -1,91 +1,11 @@
 import { user_buttons } from "../AppBar/UserButtons";
-import { TWorldCoord } from "../dimension/world_to_screen";
-import { TScrapboxItem } from "../Global/initializeGlobalState";
-import { create_new_itemid } from "../Kozane/create_new_itemid";
-import { add_item } from "./add_item";
 import { constants } from "./constants";
 import { is_touchpad } from "./is_touchpad";
 import { redraw } from "./redraw";
 import { show_dialog } from "./show_dialog";
 import { start_tutorial } from "./start_tutorial";
 import { update_style } from "./update_style";
-
-export const add_scrapbox_item_raw = (props: {
-  text: string;
-  url?: string;
-  image?: string;
-  descriptions?: string[];
-}) => {
-  const { text, image, url, descriptions } = props;
-  const scrapbox: TScrapboxItem = {
-    id: create_new_itemid(),
-    type: "scrapbox",
-    text: text,
-    image: image ?? "",
-    url: url ?? "",
-    description: descriptions ?? [],
-
-    position: [0, 0] as TWorldCoord,
-    scale: 1,
-  };
-  add_item(scrapbox);
-};
-
-export const add_scrapbox_item = (url: string) => {
-  fetch(
-    "https://us-central1-regroup-d4932.cloudfunctions.net/get_scrapbox_page",
-    {
-      method: "post",
-      body: JSON.stringify({ url }),
-    }
-  )
-    .then((x) => x.json())
-    .then((x) => {
-      console.log(x);
-      add_scrapbox_item_raw({
-        text: x.title,
-        url: url,
-        image: (x.image ?? "").replace("/raw", ""),
-        descriptions: x.descriptions,
-      });
-    });
-};
-
-type TScrapboxPage = {
-  title: string;
-  image: string | null;
-  descriptions: string[];
-};
-const foo = (x: TScrapboxPage, url?: string) => {
-  add_scrapbox_item_raw({
-    text: x.title,
-    url: url,
-    image: (x.image ?? "").replace("/raw", ""),
-    descriptions: x.descriptions,
-  });
-};
-
-export const add_scrapbox_links = (url: string) => {
-  fetch(
-    "https://us-central1-regroup-d4932.cloudfunctions.net/get_scrapbox_page",
-    {
-      method: "post",
-      body: JSON.stringify({ url }),
-    }
-  )
-    .then((x) => x.json())
-    .then((x) => {
-      console.log(x);
-      foo(x);
-      x.relatedPages.links1hop.forEach((x: TScrapboxPage) => {
-        foo(x);
-      });
-      x.relatedPages.links2hop.forEach((x: TScrapboxPage) => {
-        foo(x);
-      });
-    });
-};
-
+import { add_scrapbox_item, add_scrapbox_links } from "./make_scrapbox_kozane";
 export const kozaneba = {
   // simple values to modify
   constants,
---
+++ src/API/make_scrapbox_kozane.ts
@@ -0,0 +1,76 @@
+import { TWorldCoord } from "../dimension/world_to_screen";
+import { TScrapboxItem } from "../Global/initializeGlobalState";
+import { create_new_itemid } from "../Kozane/create_new_itemid";
+import { add_item } from "./add_item";
+
+type TScrapboxPage = {
+  title: string;
+  image: string | null;
+  descriptions: string[];
+};
+
+const make_scrapbox_kozane = (x: TScrapboxPage, url: string) => {
+  console.log(url);
+  add_scrapbox_item_raw({
+    text: x.title,
+    url: url,
+    image: (x.image ?? "").replace("/raw", ""),
+    descriptions: x.descriptions,
+  });
+};
+
+const get_page_json = (url: string) => {
+  return fetch(
+    "https://us-central1-regroup-d4932.cloudfunctions.net/get_scrapbox_page",
+    {
+      method: "post",
+      body: JSON.stringify({ url }),
+    }
+  ).then((x) => x.json());
+};
+
+const add_scrapbox_item_raw = (props: {
+  text: string;
+  url?: string;
+  image?: string;
+  descriptions?: string[];
+}) => {
+  const { text, image, url, descriptions } = props;
+  const scrapbox: TScrapboxItem = {
+    id: create_new_itemid(),
+    type: "scrapbox",
+    text: text,
+    image: image ?? "",
+    url: url ?? "",
+    description: descriptions ?? [],
+
+    position: [0, 0] as TWorldCoord,
+    scale: 1,
+  };
+  add_item(scrapbox);
+};
+
+export const add_scrapbox_item = (url: string) => {
+  get_page_json(url).then((x) => {
+    make_scrapbox_kozane(x, url);
+  });
+};
+
+const make_url = (root: string, title: string) => {
+  return root + "/" + encodeURIComponent(title);
+};
+
+export const add_scrapbox_links = (url: string) => {
+  const m = url.match(/(.*?)\/([^/]*?)$/);
+  if (m === null) return;
+  const [, root] = m;
+  get_page_json(url).then((x) => {
+    make_scrapbox_kozane(x, url);
+    x.relatedPages.links1hop.forEach((x: TScrapboxPage) => {
+      make_scrapbox_kozane(x, make_url(root!, x.title));
+    });
+    x.relatedPages.links2hop.forEach((x: TScrapboxPage) => {
+      make_scrapbox_kozane(x, make_url(root!, x.title));
+    });
+  });
+};
