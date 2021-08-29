16aca5c09f982a7d43f0f3516064bda8a3caa867
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-08-30 00:09:03 +0900

    add API on Cloud Function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,23 @@
 import * as functions from "firebase-functions";
+import fetch from "node-fetch";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+});
+
+export const get_scrapbox_page = functions.https.onRequest(
+  (request, response) => {
+    const body = JSON.parse(request.body);
+    const url = body.url;
+    const api_url = url.replace("scrapbox.io/", "scrapbox.io/api/pages/");
+    fetch(api_url).then((req) => {
+      req.text().then((text) => {
+        response.send(text);
+      });
+    });
+  }
+);
