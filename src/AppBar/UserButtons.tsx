c301adcbd93a5afcc2b165e5cf9865a4da6de7a0
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-08-31 16:46:20 +0900

    add key to fix warning

--- src/AppBar/UserButtons.tsx
+++ src/AppBar/UserButtons.tsx
@@ -7,7 +7,12 @@ export const UserButtons = () => {
   return (
     <>
       {user_buttons.map((x) => (
-        <Button color="inherit" onClick={x.onClick} className={classess.root}>
+        <Button
+          color="inherit"
+          onClick={x.onClick}
+          className={classess.root}
+          key={x.label}
+        >
           {x.label}
         </Button>
       ))}
