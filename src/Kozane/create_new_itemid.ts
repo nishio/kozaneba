00cd2735a8aa83b438b2a130e4a5c9e90e919678
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-08-31 17:08:36 +0900

    remove log

--- src/Kozane/create_new_itemid.ts
+++ src/Kozane/create_new_itemid.ts
@@ -4,18 +4,13 @@ import { ItemId } from "../Global/initializeGlobalState";
 let last_timestamp = 0;
 export const create_new_itemid = () => {
   let timestamp = Date.now();
-  console.log(timestamp);
   const fix = getGlobal().fix_timestamp_for_test;
-  console.log(timestamp);
   if (fix !== null && fix !== undefined) {
     timestamp = fix;
   }
-  console.log(timestamp);
   if (last_timestamp >= timestamp) {
     timestamp = last_timestamp + 1;
   }
-  console.log(timestamp);
   last_timestamp = timestamp;
-  console.log(timestamp);
   return timestamp.toString() as ItemId;
 };
