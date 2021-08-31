6abe02816083e569917df2fe8e397698ba470e18
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-09-01 02:12:33 +0900

    update

--- src/Event/onGroupMouseUp.ts
+++ src/Event/onGroupMouseUp.ts
@@ -1,9 +1,13 @@
 import React from "react";
+import { getGlobal } from "reactn";
+import { mark_local_changed } from "../Cloud/mark_local_changed";
 import { add_v2w, sub_v2w } from "../dimension/V2";
 import { screen_to_world } from "../dimension/world_to_screen";
 import { updateGlobal } from "../Global/updateGlobal";
 import { find_parent } from "../Group/find_parent";
 import { TGroupItem } from "../Group/GroupItem";
+import { move_front } from "../Menu/move_front";
+import { normalize_group_position } from "../Menu/normalize_group_position";
 import { remove_item_from } from "../utils/remove_item";
 import { reset_target } from "./fast_drag_manager";
 import { get_client_pos } from "./get_client_pos";
@@ -20,66 +24,61 @@ export const onGroupMouseUp = (
   if (handle_if_is_click(event)) return;
 
   const group_id = group.id;
-  // if (group_id === target_id) {
-  //   // drop a group on itself
-  //   onCanvasDrop(event);
-  //   return;
-  // }
-  updateGlobal((g) => {
-    const group = g.itemStore[group_id] as TGroupItem;
-    console.log("drag_target", g.drag_target);
-    if (g.drag_target === "selection") {
-      const delta = sub_v2w(
-        screen_to_world(get_client_pos(event)),
-        g.dragstart_position
-      );
+  move_front(group_id);
+  const g = getGlobal();
+
+  const target_id = g.drag_target;
+  if (target_id === "selection") {
+    console.log("selection drop on group", group_id);
+    const delta = sub_v2w(
+      screen_to_world(get_client_pos(event)),
+      g.dragstart_position
+    );
+    updateGlobal((g) => {
+      const group_draft = g.itemStore[group_id] as TGroupItem;
       g.selected_items.forEach((id) => {
         const x = get_item(g, id);
         x.position = sub_v2w(add_v2w(x.position, delta), group.position);
         g.drawOrder = remove_item_from(g.drawOrder, id);
-        group.items.push(id);
+        group_draft.items.push(id);
       });
-      // const sr = g.selectionRange;
-      // const [qx, qy] = world_to_screen(
-      //   add_v2(screen_to_world([sr.left, sr.top]), delta)
-      // );
-      // g.selectionRange.left = qx;
-      // g.selectionRange.top = qy;
       g.drag_target = "";
 
       // reset selection
       g.selected_items = [];
       g.selectionRange = { top: 0, left: 0, width: 0, height: 0 };
       g.is_selected = false;
-      g.is_local_change = true;
-      g.last_updated = Date.now();
-    } else if (g.drag_target !== "") {
-      console.log("drop on group", group.id);
+    });
+    normalize_group_position(group_id);
+    mark_local_changed();
+  } else if (target_id !== "") {
+    console.log("drop on group", group_id);
+    updateGlobal((g) => {
       let position = sub_v2w(
         screen_to_world(get_client_pos(event)),
         g.dragstart_position
       );
 
-      const parent = find_parent(g.drag_target);
+      const parent = find_parent(target_id);
       if (parent !== null) {
         const p = g.itemStore[parent] as TGroupItem;
         // `p` may equals to `group`, it's OK
-        p.items = remove_item_from(p.items, g.drag_target);
-        group.items.push(g.drag_target);
+        p.items = remove_item_from(p.items, target_id);
+        group.items.push(target_id);
         position = sub_v2w(add_v2w(position, p.position), group.position);
       } else {
-        g.drawOrder = remove_item_from(g.drawOrder, g.drag_target);
-        group.items.push(g.drag_target);
+        g.drawOrder = remove_item_from(g.drawOrder, target_id);
+        group.items.push(target_id);
         position = sub_v2w(position, group.position);
       }
-      const target = get_item(g, g.drag_target);
+      const target = get_item(g, target_id);
       target.position = position;
       g.drag_target = "";
-      g.is_local_change = true;
-      g.last_updated = Date.now();
-      reset_target();
-    } else {
-      throw new Error();
-    }
-  });
+    });
+    normalize_group_position(group_id);
+    mark_local_changed();
+    reset_target();
+  } else {
+    throw new Error();
+  }
 };
