b15b15f4f106ce826ade1dabf4bdfa862969ee78
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-09-01 18:55:13 +0900

    fix bug of removing child doesnot redraw, move parent front

--- src/Menu/delete_item_from_world.tsx
+++ src/Menu/delete_item_from_world.tsx
@@ -3,10 +3,12 @@ import { updateGlobal } from "../Global/updateGlobal";
 import { find_parent } from "../Group/find_parent";
 import { TGroupItem } from "../Group/GroupItem";
 import { remove_item_from } from "../utils/remove_item";
+import { move_front } from "./move_front";
 
 export const delete_item_from_world = (id: ItemId) => {
+  let parent = null;
   updateGlobal((g) => {
-    const parent = find_parent(id);
+    parent = find_parent(id);
     if (parent !== null) {
       const p = g.itemStore[parent] as TGroupItem;
       p.items = remove_item_from(p.items, id);
@@ -15,4 +17,7 @@ export const delete_item_from_world = (id: ItemId) => {
     }
     delete g.itemStore[id];
   });
+  if (parent !== null) {
+    move_front(parent);
+  }
 };
