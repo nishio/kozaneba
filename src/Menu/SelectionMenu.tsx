62ab07d9d5e2c01131b59140b23e3b9eafb8a32d
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-09-01 13:24:49 +0900

    copy as JSON

--- src/Menu/SelectionMenu.tsx
+++ src/Menu/SelectionMenu.tsx
@@ -4,6 +4,8 @@ import { getGlobal, useGlobal } from "reactn";
 import { kozaneba } from "../API/KozanebaAPI";
 import { UserMenuItem } from "../API/UserMenuItem";
 import { mark_local_changed } from "../Cloud/mark_local_changed";
+import { get_item } from "../Event/get_item";
+import { ItemId, TItem } from "../Global/initializeGlobalState";
 import { reset_selection } from "../Selection/reset_selection";
 import { copy_text } from "./copy_text";
 import { delete_item_from_world } from "./delete_item_from_world";
@@ -38,6 +40,12 @@ export const SelectionMenu = () => {
     copy_text();
     setMenu("");
   };
+
+  const onCopyJSON = () => {
+    copy_json();
+    setMenu("");
+  };
+
   return (
     <Menu anchorEl={anchor} keepMounted open={open} onClose={onClose}>
       <MenuItem onClick={onMakeGroup} data-testid="make-group">
@@ -46,10 +54,37 @@ export const SelectionMenu = () => {
       <MenuItem onClick={onCopyText} data-testid="copy-text">
         copy text
       </MenuItem>
+      <MenuItem onClick={onCopyJSON} data-testid="copy-text">
+        copy JSON
+      </MenuItem>
 
       {kozaneba.user_menus["Selection"]!.map(UserMenuItem)}
 
       <MenuItem onClick={onDelete}>delete items</MenuItem>
     </Menu>
   );
+};
+
+const copy_json = () => {
+  const g = getGlobal();
+  const drawOrder: ItemId[] = [];
+  const itemStore: { [id: string]: TItem } = {};
+
+  const _add_item = (id: ItemId) => {
+    drawOrder.push(id);
+    const item = get_item(g, id);
+    itemStore[id] = item;
+    if (item.type === "group") {
+      item.items.forEach(_add_item);
+    }
+  };
+  g.selected_items.forEach(_add_item);
+  const json = JSON.stringify({
+    drawOrder,
+    itemStore,
+    format: "Kozaneba",
+    version: 3,
+  });
+  console.log(json);
+  navigator.clipboard.writeText(json);
 };
