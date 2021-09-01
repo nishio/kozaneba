bad67163c46210a7438bc0bd456bbb52e147e126
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-09-01 19:03:55 +0900

    move samples into js file

--- src/API/sample/copy_title.ts
+++ src/API/sample/copy_title.ts
@@ -1,21 +1,6 @@
 import { kozaneba } from "../KozanebaAPI";
 import { TUserMenu } from "../UserMenuItem";
 
-/* As JavaScript
-import { kozaneba } from "./KozanebaAPI";
-export const copy_title = {
-    label: "copy title",
-    onClick: () => {
-        const item = kozaneba.get_clicked_item();
-        if (item !== undefined) {
-            navigator.clipboard.writeText(item.text);
-        }
-    },
-};
-export const onLoad = () => {
-    kozaneba.user_menus.Scrapbox.push(copy_title);
-};
-*/
 export const copy_title: TUserMenu = {
   label: "copy title",
   onClick: () => {
---
+++ src/API/sample/sample_as_javascript.js
@@ -0,0 +1,51 @@
+// you do not need to put this import in UserScript
+import { kozaneba } from "./KozanebaAPI";
+
+kozaneba.after_render_toppage = () => {
+  kozaneba.show_dialog("User");
+};
+
+kozaneba.user_buttons.push({
+  label: "Add",
+  onClick: () => {
+    kozaneba.show_dialog("AddKozane");
+  },
+});
+
+kozaneba.constants.group_padding = 5;
+
+const copy_title = {
+  label: "copy title",
+  onClick: () => {
+    const item = kozaneba.get_clicked_item();
+    if (item !== undefined) {
+      navigator.clipboard.writeText(item.text);
+    }
+  },
+};
+kozaneba.user_menus.Scrapbox.push(copy_title);
+
+const fit_to_selection = {
+  label: "fit to",
+  onClick: () => {
+    kozaneba.fit_to_contents(kozaneba.get_selected_ids());
+    kozaneba.reset_selection();
+  },
+};
+kozaneba.user_menus.Selection.push(fit_to_selection);
+
+const make_transparent = {
+  label: "make transparent",
+  onClick: () => {
+    const g = kozaneba.get_global();
+    console.log("onClick", g.clicked_target);
+    if (g.clicked_target === "") return;
+    kozaneba.update_style(g.clicked_target, (style) => {
+      style.mixBlendMode = "multiply";
+      style.border = "none";
+      return style;
+    });
+    kozaneba.redraw();
+  },
+};
+kozaneba.user_menus.Gyazo.push(make_transparent);
