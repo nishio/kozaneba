128ab2d67cb6e4ed70a6140480b88a35c45e22f8
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-09-02 11:31:22 +0900

    fix paste

--- src/App/try_to_import_json.tsx
+++ src/App/try_to_import_json.tsx
@@ -12,6 +12,7 @@ export const try_to_import_json = (text: string) => {
     const j: JSON_KozanebaV3 = JSON.parse(text);
 
     if (j.format === "Kozaneba" && j.version === 3) {
+      const to_add: TItem[] = [];
       const visit = (old_id: string): ItemId => {
         const id = create_new_itemid();
         const item = j.itemStore[old_id]!;
@@ -19,10 +20,15 @@ export const try_to_import_json = (text: string) => {
         if (new_item.type === "group") {
           new_item.items = new_item.items.map(visit);
         }
-        add_item(new_item);
+        to_add.push(new_item);
         return id;
       };
       const new_items = j.drawOrder.map(visit);
+      updateGlobal((g) => {
+        to_add.forEach((item) => {
+          g.itemStore[item.id] = item;
+        });
+      });
       const group = new GroupItem();
       group.items = new_items;
       add_item(group);
