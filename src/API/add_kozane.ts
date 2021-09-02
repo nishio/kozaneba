bb84410904c216021f8d7da6606977fce61e39a1
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-09-02 14:04:41 +0900

    make URL-kozane on paste

--- src/API/add_kozane.ts
+++ src/API/add_kozane.ts
@@ -1,10 +1,11 @@
 import { get_center_of_screen } from "../Dialog/AddKozaneDialog/get_center_of_screen";
-import { KozaneItem } from "../Kozane/KozaneItem";
+import { KozaneItem, TKozaneItem } from "../Kozane/KozaneItem";
 import { add_item } from "./add_item";
 
-export const add_kozane = (text: string) => {
+export const add_kozane = (text: string, option: Partial<TKozaneItem> = {}) => {
   const kozane = new KozaneItem();
   kozane.text = text;
   kozane.position = get_center_of_screen();
+  Object.assign(kozane, option);
   add_item(kozane);
 };
--- src/App/onPaste.tsx
+++ src/App/onPaste.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { add_kozane } from "../API/add_kozane";
 import { add_scrapbox_item } from "../API/make_scrapbox_kozane";
 import { updateGlobal } from "../Global/updateGlobal";
 import { add_gyazo_item } from "./add_gyazo_item";
@@ -19,11 +20,20 @@ export const onPaste = (e: React.ClipboardEvent) => {
   navigator.clipboard.readText().then(pasted);
 };
 
+const url_to_text = (url: string) => {
+  const u = new URL(url);
+  const items = u.pathname.split("/");
+  const last = items[items.length - 1] ?? items[items.length - 2] ?? u.hostname;
+  return decodeURIComponent(last);
+};
+
 const pasted = (text: string) => {
   if (text.startsWith("https://scrapbox.io/")) {
     add_scrapbox_item(text);
   } else if (text.startsWith("https://gyazo.com/")) {
     add_gyazo_item(text);
+  } else if (text.startsWith("https://") || text.startsWith("http://")) {
+    add_kozane(url_to_text(text), { custom: { url: text } });
   } else if (try_to_import_json(text)) {
     // if true(=success) do nothing
   } else {
