d15a3c5ae0780894f0c211c5ea284473b58888f0
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-09-03 12:16:26 +0900

    show/save back up

--- src/Cloud/LocalBackup.ts
+++ src/Cloud/LocalBackup.ts
@@ -1,4 +1,7 @@
 import Dexie from "dexie";
+import { getGlobal } from "reactn";
+import { date_to_str } from "../utils/date_to_str";
+import { save_new } from "./initial_save";
 
 export interface IBackup {
   id?: number; // Primary key. Optional (autoincremented)
@@ -15,8 +18,8 @@ class MyAppDatabase extends Dexie {
 
   constructor() {
     super("Kozaneba");
-    this.version(1).stores({
-      backup: "++id",
+    this.version(2).stores({
+      backup: "++id,cloud_ba,last_updated",
       //...other tables goes here...
     });
     // The following line is needed if your typescript
@@ -25,95 +28,41 @@ class MyAppDatabase extends Dexie {
   }
 }
 
-// export const loadRaw = async (): Promise<string | null> => {
-//   const x = await localDB.saves.orderBy("id").reverse().limit(1).toArray();
-//   if (x.length === 0) {
-//     return null;
-//   } else {
-//     return x[0].json;
-//   }
-// };
+// @ts-ignore
+window.kozaneba.show_backup = () => {
+  const g = getGlobal();
+  local_db.backup
+    .where("cloud_ba")
+    .equals(g.cloud_ba)
+    .toArray()
+    .then((xs) => {
+      console.table(
+        xs.map((x) => {
+          return {
+            id: x.id,
+            last_updated: date_to_str(x.last_updated),
+            size: x.json.length,
+          };
+        })
+      );
+    });
+};
 
-// const loadJSON = (json: string | null) => {
-//   if (json !== null) {
-//     const value = JSON.parse(json);
-//     // rename
-//     if (value.converters.furnace_for_iron) {
-//       value.converters.furnace =
-//         (value.converters.furnace ?? 0) +
-//         (value.converters.furnace_for_iron ?? 0);
-//       value.converters.furnace_for_iron = undefined;
-//     }
-//     if (value.converters.workbench_for_iron_pickaxe) {
-//       value.converters.workbench =
-//         (value.converters.workbench ?? 0) +
-//         (value.converters.workbench_for_iron_pickaxe ?? 0);
-//       value.converters.workbench_for_iron_pickaxe = undefined;
-//     }
-//     checkLoadData(value);
-//     return setGlobal(value);
-//   }
-// };
+// @ts-ignore
+window.kozaneba.save_backup_as_new = (id: number, open = true) => {
+  local_db.backup
+    .where("id")
+    .equals(id)
+    .first()
+    .then((x) => {
+      if (x === undefined) {
+        console.error("not found");
+        return;
+      }
 
-// export const load = () => {
-//   return loadRaw().then(loadJSON);
-// };
-
-// export const save = (): Promise<unknown> => {
-//   checkSaveData();
-
-//   const g = getGlobal();
-//   return localDB.saves
-//     .orderBy("id")
-//     .reverse()
-//     .limit(1)
-//     .toArray()
-//     .then((x) => {
-//       if (x.length === 0) {
-//         return localDB.saves.add({ json: JSON.stringify(g) });
-//       } else {
-//         return localDB.saves.update(x[0].id!, { json: JSON.stringify(g) });
-//       }
-//     })
-//     .then(() => {
-//       const today = getDateStr();
-//       return localDB.dailyBackup.put(
-//         {
-//           id: today,
-//           json: JSON.stringify(g),
-//         },
-//         today
-//       );
-//     });
-// };
-
-// export const recover = (): Promise<unknown> => {
-//   return localDB.dailyBackup
-//     .orderBy("id")
-//     .reverse()
-//     .limit(2)
-//     .toArray()
-//     .then((xs) => {
-//       const today = getDateStr();
-//       let json: string;
-//       if (xs.length === 0) {
-//         alert("no data to recover");
-//         return;
-//       } else if (xs.length === 1) {
-//         if (xs[0].id === today) {
-//           alert("no data to recover");
-//           return;
-//         }
-//         json = xs[0].json;
-//       } else {
-//         if (xs[0].id === today) {
-//           json = xs[1].json;
-//         } else {
-//           json = xs[0].json;
-//         }
-//       }
-//       return loadJSON(json);
-//     });
-// };
+      const docdata = JSON.parse(x.json);
+      save_new(docdata);
+    });
+};
 
 export const local_db = new MyAppDatabase();
