83427102b6994991e522fb5aff8663be444a0cf1
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-09-06 13:57:30 +0900

    line and box crosspoint

--- src/Canvas/ItemCanvas.tsx
+++ src/Canvas/ItemCanvas.tsx
@@ -11,6 +11,11 @@ import { ItemId } from "../Global/initializeGlobalState";
 import { useEffect, useRef } from "react";
 import { onWheel } from "../Event/onWheel";
 import { dev_log, dev_time, dev_time_end } from "../utils/dev";
+import { add_v2, sub_v2, V2 } from "../dimension/V2";
+import { TRect } from "../dimension/TRect";
+import { add_kozane } from "../API/add_kozane";
+import { get_item_bounding_box } from "../dimension/get_bounding_box";
+import { TBoundingBox } from "../dimension/TBoundingBox";
 
 export const ItemCanvas = () => {
   const [kozane] = useGlobal("kozane");
@@ -78,13 +83,31 @@ export const ItemCanvas = () => {
   );
 };
 
+const bounding_box_to_rect = (bb: TBoundingBox): TRect => {
+  const { left, top, right, bottom } = bb;
+  return {
+    left,
+    top,
+    width: right - left,
+    height: bottom - top,
+  };
+};
 const SVGLayer = () => {
   const [g] = useGlobal();
-  const [x1, y1] = g.itemStore["1"]!.position;
-  const [x2, y2] = g.itemStore["2"]!.position;
+  const v1 = g.itemStore["1"]!.position;
+  const v2 = g.itemStore["2"]!.position;
   const WIDTH = 500;
   const HEIGHT = 500;
-
+  const [x1, y1] = get_box_line_crosspoint(
+    v1,
+    v2,
+    bounding_box_to_rect(get_item_bounding_box("1" as ItemId))
+  );
+  const [x2, y2] = get_box_line_crosspoint(
+    v2,
+    v1,
+    bounding_box_to_rect(get_item_bounding_box("2" as ItemId))
+  );
   return (
     <svg
       version="1.1"
@@ -99,7 +122,15 @@ const SVGLayer = () => {
       }}
       viewBox={`-${WIDTH / 2} -${HEIGHT / 2} ${WIDTH} ${HEIGHT}`}
     >
-      <line x1={x1} y1={y1} x2={x2} y2={y2} stroke="black" strokeWidth="10" />
+      <line
+        x1={x1}
+        y1={y1}
+        x2={x2}
+        y2={y2}
+        stroke="black"
+        strokeWidth="10"
+        strokeLinecap="round"
+      />
 
       <text x="-100" y="-100" font-size="30" text-anchor="middle" fill="black">
         SVG
@@ -112,4 +143,35 @@ const SVGLayer = () => {
       </text>
     </svg>
   );
+};
+
+const get_box_line_crosspoint = (start: V2, end: V2, box: TRect) => {
+  // box is around the start point
+  const [dx, dy] = sub_v2(end, start);
+  var grad = dy / dx;
+  var igrad = dx / dy;
+
+  const w = box.width / 2;
+  const h = box.height / 2;
+  let crosspoint: V2;
+  if (w * Math.abs(dy) > h * Math.abs(dx)) {
+    //intersection is on the top or bottom
+    if (dy > 0) {
+      // top
+      crosspoint = [h * igrad, h];
+    } else {
+      // bottom
+      crosspoint = [-h * igrad, -h];
+    }
+  } else {
+    //intersection is on the left or right
+    if (dx > 0) {
+      // right
+      crosspoint = [w, w * grad];
+    } else {
+      // left
+      crosspoint = [-w, -grad * w];
+    }
+  }
+  return add_v2(start, crosspoint);
 };
