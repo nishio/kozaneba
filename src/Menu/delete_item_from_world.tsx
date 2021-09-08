fe19a315929357ff9acb4fb9c1c1b8c7f148d6bc
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-09-08 16:37:46 +0900

    remove recursively, and update annotations

--- src/Menu/delete_item_from_world.tsx
+++ src/Menu/delete_item_from_world.tsx
@@ -1,4 +1,8 @@
+import { getGlobal } from "reactn";
+import { State } from "reactn/default";
+import { get_item } from "../Event/get_item";
 import { ItemId } from "../Global/initializeGlobalState";
+import { TAnnotation } from "../Global/TAnnotation";
 import { updateGlobal } from "../Global/updateGlobal";
 import { find_parent } from "../Group/find_parent";
 import { TGroupItem } from "../Group/GroupItem";
@@ -7,6 +11,11 @@ import { move_front } from "./move_front";
 
 export const delete_item_from_world = (id: ItemId) => {
   let parent = null;
+  const item = get_item(getGlobal(), id);
+  if (item.type === "group") {
+    item.items.forEach(delete_item_from_world);
+  }
+
   updateGlobal((g) => {
     parent = find_parent(id);
     if (parent !== null) {
@@ -16,8 +25,22 @@ export const delete_item_from_world = (id: ItemId) => {
       g.drawOrder = remove_item_from(g.drawOrder, id);
     }
     delete g.itemStore[id];
+    update_annotation_after_deletion(g, id);
   });
   if (parent !== null) {
     move_front(parent);
   }
+};
+
+const update_annotation_after_deletion = (g: State, id: ItemId) => {
+  const new_annotation: TAnnotation[] = [];
+  g.annotations.forEach((a) => {
+    a.items = remove_item_from(a.items, id);
+    if (a.type === "line" && a.items.length < 2) {
+      // delete
+      return;
+    }
+    new_annotation.push(a);
+  });
+  g.annotations = new_annotation;
 };
