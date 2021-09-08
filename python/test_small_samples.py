c538dd072a9d4f372f6c29a0f1d7117edffba08e
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-09-08 16:36:40 +0900

    add test

--- python/test_small_samples.py
+++ python/test_small_samples.py
@@ -22,3 +22,20 @@ def test_sample_group():
     expected = json.load(open("small_sample_group.json"))
     actual = json.loads(ba.to_json())
     assert expected == actual
+
+
+def test_sample_group():
+    ba = kozaneba.Ba()
+    items = []
+    items.append(ba.make_kozane("1", [-100, 0]))
+    items.append(ba.make_kozane("2", [100, 0]))
+    ba.add_group(items, [0, 0], "G1")
+    ba.add_simple_arrow("1", "2")
+
+    expected = json.load(open("small_sample_group_arrow.json"))
+    actual = json.loads(ba.to_json())
+    assert expected == actual
+
+
+def save_json(ba, name):
+    open(name, "w").write(ba.to_json())
