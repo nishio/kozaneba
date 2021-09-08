72994681100e2eeb67cda4c7a9980d56b0c7f0b9
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-09-08 13:03:58 +0900

    if parent group is closed, use its position

--- src/Canvas/get_global_position.tsx
+++ src/Canvas/get_global_position.tsx
@@ -1,5 +1,6 @@
 import { State } from "reactn/default";
 import { add_v2, V2 } from "../dimension/V2";
+import { get_group } from "../Event/get_group";
 import { get_item } from "../Event/get_item";
 import { ItemId } from "../Global/initializeGlobalState";
 import { find_parent } from "../Group/find_parent";
@@ -9,7 +10,12 @@ export const get_global_position = (id: ItemId, g: State): V2 => {
   let v: V2 = get_item(g, id).position;
   let p = find_parent(id);
   while (p !== null) {
-    v = add_v2(v, get_item(g, p).position);
+    const group = get_group(g, p);
+    if (group.isOpen) {
+      v = add_v2(v, get_item(g, p).position);
+    } else {
+      v = group.position;
+    }
     p = find_parent(p);
   }
   return v;
