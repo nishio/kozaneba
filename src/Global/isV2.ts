6ee1d20bbeb050d3ce75c1e9714b988c2217873c
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-09-09 13:17:33 +0900

    fix bug

--- src/Global/isV2.ts
+++ src/Global/isV2.ts
@@ -1,5 +1,5 @@
 import { V2 } from "../dimension/V2";
 
 export function isV2(x: any): x is V2 {
-  return Array.isArray(x.position) && x.position.length === 2;
+  return Array.isArray(x) && x.length === 2;
 }
