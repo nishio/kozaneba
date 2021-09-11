4292d846f52c42b6b782218f476df1403df1625d
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-09-12 03:44:26 +0900

    `add biipartite graph` menu

---
+++ src/API/sample/add_bipartite.tsx
@@ -0,0 +1,24 @@
+import { ItemId } from "../../Global/ItemId";
+import { kozaneba } from "../KozanebaAPI";
+
+export const add_bipartite = (targets: ItemId[]) => {
+  if (targets.length < 2) return;
+  const positions = targets.map((id) => kozaneba.get_item(id).position);
+  const gp = kozaneba.get_gravity_point(positions);
+  const lefts = targets.filter((_, index) => positions[index]![0] <= gp[0]);
+  const rights = targets.filter((_, index) => positions[index]![0] > gp[0]);
+  lefts.forEach((left) => {
+    rights.forEach((right) => {
+      kozaneba.add_arrow([left, right], ["none", "arrow"]);
+    });
+  });
+};
+
+kozaneba.user_menus.Selection!.push({
+  label: "add bipartite graph",
+  onClick: () => {
+    add_bipartite(kozaneba.get_selected_ids());
+    kozaneba.reset_selection();
+    kozaneba.redraw();
+  },
+});
