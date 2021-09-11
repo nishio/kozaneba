89ae30a9992dea00bfe3637c16193cead51cff53
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-09-12 03:43:36 +0900

    add _arrow API

---
+++ src/API/add_arrow.ts
@@ -0,0 +1,23 @@
+import { ItemId } from "../Global/ItemId";
+import { TArrowHead } from "../Global/TAnnotation";
+import { updateGlobal } from "../Global/updateGlobal";
+import { mark_local_changed } from "../utils/mark_local_changed";
+
+const THICK_ARROW = {
+  stroke_width: 10,
+  arrow_head_size: 30,
+  opacity: 1,
+  is_clickable: true,
+};
+
+export const add_arrow = (ids: ItemId[], heads: TArrowHead[], custom = {}) => {
+  updateGlobal((g) => {
+    g.annotations.push({
+      type: "line",
+      items: ids,
+      heads: heads,
+      custom: THICK_ARROW,
+    });
+  });
+  mark_local_changed();
+};
