7ec13001493f552949356e85395f2fc9a3ede27f
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-09-13 17:57:59 +0900

    add kintone API

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from "firebase-functions";
 import fetch from "node-fetch";
+const { KintoneRestAPIClient } = require("@kintone/rest-api-client");
 
 export const get_scrapbox_page = functions.https.onRequest((req, res) => {
   res.set("Access-Control-Allow-Origin", "*");
@@ -20,5 +21,54 @@ export const get_scrapbox_page = functions.https.onRequest((req, res) => {
     req.text().then((text) => {
       res.send(text);
     });
+  });
+});
+
+export const proxy = functions.https.onRequest((req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+
+  if (req.method === "OPTIONS") {
+    // Send response to preflight OPTIONS requests
+    res.set("Access-Control-Allow-Methods", "GET");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Max-Age", "3600");
+    res.status(204).send("");
+    return;
+  }
+
+  const body = JSON.parse(req.body);
+  const url = body.url;
+  const method = req.method;
+  const fetch_param = {
+    ...body,
+    method,
+  };
+  fetch(url, fetch_param).then((req) => {
+    req.text().then((text) => {
+      res.send(text);
+    });
+  });
+});
+
+export const kintone_get = functions.https.onRequest((req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+
+  if (req.method === "OPTIONS") {
+    // Send response to preflight OPTIONS requests
+    res.set("Access-Control-Allow-Methods", "GET");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Max-Age", "3600");
+    res.status(204).send("");
+    return;
+  }
+
+  const body = JSON.parse(req.body);
+  const { baseUrl, apiToken, app } = body;
+  const client = new KintoneRestAPIClient({
+    baseUrl,
+    auth: { apiToken },
+  });
+  client.record.getRecords({ app }).then((resp: any) => {
+    res.send(JSON.stringify(resp.records));
   });
 });
