1757415e8d2d38d30204d0e99cf13a6fe0d0163c
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-09-22 17:12:45 +0900

    refactor: TKozaneItem as Runtypes

--- src/Global/TKozaneItem.ts
+++ src/Global/TKozaneItem.ts
@@ -1,48 +1,31 @@
-import { CSSProperties } from "react";
 import {
   Dictionary,
   Literal,
   Number,
   Optional,
-  Partial,
   Record,
   Static,
   String,
-  Tuple,
-  Union,
 } from "runtypes";
-import { create } from "runtypes/lib/runtype";
-import { TWorldCoord } from "../dimension/world_to_screen";
+import { RTWorldCoord } from "../dimension/world_to_screen";
 import { isV2 } from "./isV2";
-import { TItemId } from "./TItemId";
+import { RTItemId } from "./TItemId";
 
-export type TKozaneItem = {
-  type: "kozane";
-  text: string;
-  position: TWorldCoord;
-  id: TItemId;
-  scale: number;
-  custom?: {
-    style?: CSSProperties;
-    url?: string;
-  };
-};
+export const RTKozaneItem = Record({
+  type: Literal("kozane"),
+  text: String,
+  position: RTWorldCoord,
+  id: RTItemId,
+  scale: Number,
+  custom: Optional(
+    Record({
+      style: Dictionary(String.Or(Number)).optional(),
+      url: String.optional(),
+    })
+  ),
+});
 
-// const RTKozaneItem = Record({
-//   type: Literal("kozane"),
-//   text: String,
-//   position: Tuple(Number, Number),
-//   id: String,
-//   scale: Number,
-//   custom: Optional(
-//     Record({
-//       style: Dictionary(Union(String, Number), String).optional(),
-//       url: String.optional(),
-//     })
-//   ),
-// });
-
-// export type TKozaneItem = Static<typeof RTKozaneItem>;
+export type TKozaneItem = Static<typeof RTKozaneItem>;
 
 export function isTKozaneItem(x: any): x is TKozaneItem {
   return (
