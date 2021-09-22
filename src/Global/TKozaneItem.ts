a56ea657ce2155cd2b2cd653081d1a33a3352783
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-09-22 17:22:31 +0900

    refactor: remove isTKozaneItem

--- src/Global/TKozaneItem.ts
+++ src/Global/TKozaneItem.ts
@@ -8,7 +8,6 @@ import {
   String,
 } from "runtypes";
 import { RTWorldCoord } from "../dimension/world_to_screen";
-import { isV2 } from "./isV2";
 import { RTItemId } from "./TItemId";
 
 export const RTKozaneItem = Record({
@@ -26,13 +25,3 @@ export const RTKozaneItem = Record({
 });
 
 export type TKozaneItem = Static<typeof RTKozaneItem>;
-
-export function isTKozaneItem(x: any): x is TKozaneItem {
-  return (
-    x.type === "kozane" &&
-    typeof x.text === "string" &&
-    isV2(x.position) &&
-    typeof x.id === "string" &&
-    typeof x.scale === "number"
-  );
-}
--- src/Global/isTItem.ts
+++ src/Global/isTItem.ts
@@ -1,11 +1,14 @@
 import { isTGroupItem } from "./TGroupItem";
 import { isTGyazoItem } from "./TGyazoItem";
 import { TItem } from "./TItem";
-import { isTKozaneItem } from "./TKozaneItem";
+import { RTKozaneItem } from "./TKozaneItem";
 import { isTScrapboxItem } from "./TScrapboxItem";
 
 export function isTItem(x: any): x is TItem {
   return (
-    isTKozaneItem(x) || isTGroupItem(x) || isTScrapboxItem(x) || isTGyazoItem(x)
+    RTKozaneItem.guard(x) ||
+    isTGroupItem(x) ||
+    isTScrapboxItem(x) ||
+    isTGyazoItem(x)
   );
 }
