59d8d1b8b01a29b1c725470596d39323f1ddc24e
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-09-28 17:28:47 +0900

    add arrow menu as image

--- src/Menu/SelectionMenu.tsx
+++ src/Menu/SelectionMenu.tsx
@@ -2,6 +2,7 @@ import { Menu, MenuItem } from "@material-ui/core";
 import React from "react";
 import { getGlobal, useGlobal } from "reactn";
 import { kozaneba } from "../API/KozanebaAPI";
+import { add_bipartite } from "../API/sample/add_bipartite";
 import { UserMenuItem } from "../API/UserMenuItem";
 import { reset_selection } from "../Selection/reset_selection";
 import { add_arrow } from "../utils/add_arrow";
@@ -58,6 +59,12 @@ export const SelectionMenu = () => {
     mark_local_changed();
     setMenu("");
   };
+  const onAddBipartite = () => {
+    add_bipartite(getGlobal().selected_items);
+    reset_selection();
+    mark_local_changed();
+    setMenu("");
+  };
 
   return (
     <Menu anchorEl={anchor} keepMounted open={open} onClose={onClose}>
@@ -70,12 +77,52 @@ export const SelectionMenu = () => {
       <MenuItem onClick={onCopyJSON} data-testid="copy-text">
         copy JSON
       </MenuItem>
-      <MenuItem onClick={onAddArrow} data-testid="copy-text">
+
+      <li
+        style={{
+          paddingLeft: "16px",
+          paddingTop: "6px",
+          borderTop: "solid 1px #eee",
+        }}
+      >
+        Add Arrow
+      </li>
+      <div
+        style={{
+          display: "flex",
+          padding: "6px 16px",
+          borderBottom: "solid 1px #eee",
+        }}
+      >
+        <MenuItem onClick={onAddArrow} dense={true} disableGutters={true}>
+          <img
+            src="https://gyazo.com/75a5ad73d5d4b768de7bf1f3ea7864c2/thumb/50"
+            alt="add arrow(head:left)"
+          />
+        </MenuItem>
+        <MenuItem
+          onClick={onAddArrowAllHeads}
+          dense={true}
+          disableGutters={true}
+        >
+          <img
+            src="https://gyazo.com/770e188862ebe1db8ceacb691a973eaf/thumb/50"
+            alt="add arrow(head:all)"
+          />
+        </MenuItem>
+        <MenuItem onClick={onAddBipartite} dense={true} disableGutters={true}>
+          <img
+            src="https://gyazo.com/f95bb0b29f554dc5cff8f50562c26e67/thumb/50"
+            alt="add arrow(bipartite)"
+          />
+        </MenuItem>
+      </div>
+      {/* <MenuItem onClick={onAddArrow} data-testid="copy-text">
         add arrow(head:left)
       </MenuItem>
       <MenuItem onClick={onAddArrowAllHeads} data-testid="copy-text">
         add arrow(head:all)
-      </MenuItem>
+      </MenuItem> */}
 
       {kozaneba.user_menus["Selection"]!.map(UserMenuItem)}
 
