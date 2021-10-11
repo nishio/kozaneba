8b3522ec4d847b07bdf55383255d545c7a5ed248
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-10-11 17:57:52 +0900

    runtypes

--- src/Global/TAnnotation.ts
+++ src/Global/TAnnotation.ts
@@ -1,27 +1,22 @@
-import { TItemId } from "./TItemId";
+import { Array, Boolean, Literal, Number, Record, Static } from "runtypes";
+import { RTItemId } from "./TItemId";
 
+export const RTArrowHead = Literal("none").Or(Literal("arrow"));
+export type TArrowHead = Static<typeof RTArrowHead>;
+
+export const RTLineAnnot = Record({
+  type: Literal("line"),
+  items: Array(RTItemId),
+  heads: Array(RTArrowHead),
+  is_doubled: Boolean,
+  custom: Record({
+    arrow_head_size: Number.optional(),
+    is_clickable: Boolean.optional(),
+    stroke_width: Number.optional(),
+    opacity: Number.optional(),
+  }),
+});
+export type TLineAnnot = Static<typeof RTLineAnnot>;
+
+export const RTAnnotation = RTLineAnnot;
 export type TAnnotation = TLineAnnot;
-
-export type TLineAnnot = {
-  type: "line";
-  items: TItemId[];
-  heads: TArrowHead[];
-  custom?: {
-    arrow_head_size?: number;
-    is_clickable?: boolean;
-    stroke_width?: number;
-    opacity?: number;
-  };
-};
-
-export type TArrowHead = "none" | "arrow";
-
-export function isTAnnotation(x: any): x is TAnnotation {
-  return (
-    x.type === "line" &&
-    Array.isArray(x.items) &&
-    x.items.every((y: any) => typeof y === "string") &&
-    Array.isArray(x.heads) &&
-    x.heads.every((y: any) => y === "none" || y === "arrow")
-  );
-}
