5edda26ff6abe9db0442b6d6738588eb5a3ad8d1
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2022-03-24 16:33:13 +0900

    parse as scarpbox

--- src/Kozane/Kozane.tsx
+++ src/Kozane/Kozane.tsx
@@ -1,6 +1,8 @@
 import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Line, parse } from "@progfay/scrapbox-parser";
 import React, { CSSProperties } from "react";
+import { getGlobal } from "reactn";
 import { TOffset } from "../dimension/TOffset";
 import { onKozaneMouseDown } from "../Event/onKozaneMouseDown";
 import { TKozaneItem } from "../Global/TKozaneItem";
@@ -56,7 +58,50 @@ export const Kozane: React.FC<Props> = ({
       onMouseDown={onMouseDown}
     >
       {link_mark()}
-      <KozaneDiv2>{value.text}</KozaneDiv2>
+      <KozaneDiv2>{build_content(value)}</KozaneDiv2>
     </KozaneDiv>
   );
+};
+
+const build_content = (value: TKozaneItem) => {
+  const g = getGlobal();
+  const proj = g.scrapbox;
+  if (proj === "") {
+    return value.text; // simple string
+  } else {
+    const page = parse("DUMMY_TITLE\n" + value.text);
+    const line = page[1] as Line;
+    return line.nodes.map((node) => {
+      if (node.type === "plain") {
+        return node.text;
+      } else if (node.type === "icon") {
+        if (node.pathType === "relative") {
+          const src = `https://scrapbox.io/api/pages/${proj}/${node.path}/icon`;
+          return <img src={src} style={{ height: "1em" }} />;
+        } else if (node.pathType === "root") {
+          const src = `https://scrapbox.io/api/pages/${node.path}/icon`;
+          return <img src={src} style={{ height: "1em" }} />;
+        } else {
+          console.log(node);
+        }
+      } else if (node.type === "link") {
+        console.log(node);
+        if (node.pathType === "relative") {
+          const src = `https://scrapbox.io/${proj}/${node.href}`;
+          // return <a href={src}> {node.raw} </a>;
+          return <span style={{ color: "blue" }}>{node.raw}</span>;
+        } else if (node.pathType === "root") {
+          const src = `https://scrapbox.io/api/pages/${node.href}`;
+          // return <a href={src}> {node.raw} </a>;
+          return <span style={{ color: "blue" }}>{node.raw}</span>;
+        } else {
+          console.log(node);
+          return node.raw;
+        }
+      } else {
+        console.log(node);
+        return node.raw;
+      }
+    });
+  }
 };
