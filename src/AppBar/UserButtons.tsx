cdcdd77d759c192889d89b28de5ca1db70e3a9a5
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2022-03-24 15:08:21 +0900

    clean

--- src/AppBar/UserButtons.tsx
+++ src/AppBar/UserButtons.tsx
@@ -7,7 +7,6 @@ export const UserButtons = () => {
         <Button
           color="inherit"
           onClick={x.onClick}
-          // className={classess.root}
           key={x.label}
           className="user-button"
         >
