4d94ed9b96fab545cdc9e0947ab11082f2bf656a
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2022-05-26 15:26:28 +0900

    make arrow as group

--- src/Canvas/Annotation/LineAnnot.tsx
+++ src/Canvas/Annotation/LineAnnot.tsx
@@ -35,10 +35,20 @@ export const LineAnnot = (g: State, a: TLineAnnot, annot_index: number) => {
     (a.custom?.arrow_head_size ?? 10) +
     (is_doubled ? stroke_width / arrow_sin : 0);
 
+  let group_opacity = 1.0;
+  const opacity_from_length = (length: number): number => {
+    const threshold = 130;
+    if (length < threshold) {
+      return 1;
+    } else if (length < threshold * 3) {
+      return 0.3;
+    }
+    return 0.1;
+  };
+
   const draw_arrowhead = (
     h: "none" | "arrow" | undefined,
-    index: number,
-    opacity = 1
+    index: number
   ): void => {
     if (h === "arrow") {
       const p = crosspoints[index];
@@ -48,10 +58,8 @@ export const LineAnnot = (g: State, a: TLineAnnot, annot_index: number) => {
       const h1 = sub_v2(p, mul_v2(arrow_head_size, rotate(n, arrow_angle)));
       const h2 = sub_v2(p, mul_v2(arrow_head_size, rotate(n, -arrow_angle)));
 
-      // lines.push([h1, p, 1]);
-      // lines.push([h2, p, 1]);
-      lines.push([h1, p, opacity]);
-      lines.push([h2, p, opacity]);
+      lines.push([h1, p, 1]);
+      lines.push([h2, p, 1]);
     }
   };
 
@@ -83,16 +91,8 @@ export const LineAnnot = (g: State, a: TLineAnnot, annot_index: number) => {
     }
     const v = sub_v2(cp1, cp0);
     const length = L2norm(v);
-    const opacity_from_length = (length: number): number => {
-      const threshold = 130;
-      if (length < threshold) {
-        return 1;
-      } else if (length < threshold * 3) {
-        return 0.3;
-      }
-      return 0.1;
-    };
-    const opacity = opacity_from_length(length);
+    const opacity = 1;
+    group_opacity = Math.min(opacity_from_length(length), group_opacity);
     if (!is_doubled) {
       lines.push([cp0, cp1, opacity]);
     } else {
@@ -106,9 +106,11 @@ export const LineAnnot = (g: State, a: TLineAnnot, annot_index: number) => {
       lines.push([add_v2(p1, d), add_v2(p0, d), opacity]);
       lines.push([sub_v2(p1, d), sub_v2(p0, d), opacity]);
     }
-    draw_arrowhead(a.heads[0], 0, opacity);
-    draw_arrowhead(a.heads[1], 1, opacity);
+    draw_arrowhead(a.heads[0], 0);
+    draw_arrowhead(a.heads[1], 1);
+    // end of (a.items.length === 2)
   } else {
+    // a.items.length !== 2
     const angles = crosspoints.flatMap((cp) => {
       if (cp === undefined) return [];
       const d = sub_v2(center, cp);
@@ -122,9 +124,15 @@ export const LineAnnot = (g: State, a: TLineAnnot, annot_index: number) => {
         draw_arrowhead(a.heads[index], index);
         if (!is_doubled) {
           lines.push([center, cp, 1]);
+          const d = sub_v2(center, cp);
+          const length = L2norm(d);
+          group_opacity = Math.min(opacity_from_length(length), group_opacity);
         } else {
           // double lines
           const d = sub_v2(center, cp);
+          const length = L2norm(d);
+          group_opacity = Math.min(opacity_from_length(length), group_opacity);
+
           const n = normalize(d);
           const side = mul_v2(stroke_width, rotate(n, 90));
           const shorten = mul_v2(stroke_width / arrow_tan, n);
@@ -165,7 +173,7 @@ export const LineAnnot = (g: State, a: TLineAnnot, annot_index: number) => {
     };
   }
 
-  const opacity = a.custom?.opacity ?? 0.2;
+  const custom_opacity = a.custom?.opacity ?? 0.2;
 
   const result = lines.map(([p1, p2, op], index) => {
     return Line(
@@ -174,10 +182,11 @@ export const LineAnnot = (g: State, a: TLineAnnot, annot_index: number) => {
       onClick,
       `line-${annot_index}-${index}`,
       stroke_width,
-      opacity * op,
+      1, // was: opacity * op,
       is_clickable
     );
   });
 
-  return result;
+  // return result;
+  return <g opacity={custom_opacity * group_opacity}>{result}</g>;
 };
