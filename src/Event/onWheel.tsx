0844250c074cb40f3d43732655e775d5e2b1cf39
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2022-06-09 14:43:49 +0900

    requestIdleCallback(disabled)

--- src/Event/onWheel.tsx
+++ src/Event/onWheel.tsx
@@ -31,31 +31,76 @@ export const zoom_around_pointer = (delta_scale: number) => {
   });
 };
 
-const zoom = (e: WheelEvent) => {
+const zoom = (e: WheelEvent, v: number) => {
   let speed = constants.wheel_scale_speed;
   if (kozaneba.is_touchpad(e)) {
     speed = constants.touchpad_scale_speed;
   }
 
-  let v = e.deltaY;
   const scale = Math.exp((-v * speed) / 10000);
   zoom_around_pointer(scale);
 };
 
+let count = 0;
+let prevCount = 0;
+let prevTiming = Date.now();
+let pendingTask: number | null = null;
+let deltaY = 0;
+let deltaX = 0;
+const toRequestIC = false;
 export const onWheel = (e: WheelEvent) => {
   e.preventDefault();
+  count++;
+  const now = Date.now();
+  if (now > prevTiming + 1000) {
+    console.log(`count: ${count - prevCount}, time: ${now - prevTiming}`);
+    prevCount = count;
+    prevTiming = now;
+  }
   const speed = constants.wheel_move_speed;
-  if (e.ctrlKey) {
-    zoom(e);
-  } else if (e.shiftKey) {
-    updateGlobal((g) => {
-      g.trans_x -= (e.deltaY / g.scale) * speed;
-      g.trans_y -= (e.deltaX / g.scale) * speed;
-    });
+
+  if (toRequestIC) {
+    deltaX += e.deltaX;
+    deltaY += e.deltaY;
+    if (pendingTask !== null) {
+      cancelIdleCallback(pendingTask);
+    }
+    pendingTask = requestIdleCallback(
+      () => {
+        if (e.ctrlKey) {
+          zoom(e, deltaY);
+        } else if (e.shiftKey) {
+          updateGlobal((g) => {
+            g.trans_x -= (deltaY / g.scale) * speed;
+            g.trans_y -= (deltaX / g.scale) * speed;
+          });
+        } else {
+          updateGlobal((g) => {
+            g.trans_x -= (deltaX / g.scale) * speed;
+            g.trans_y -= (deltaY / g.scale) * speed;
+          });
+        }
+        pendingTask = null;
+        deltaX = 0;
+        deltaY = 0;
+      },
+      {
+        timeout: 100,
+      }
+    );
   } else {
-    updateGlobal((g) => {
-      g.trans_x -= (e.deltaX / g.scale) * speed;
-      g.trans_y -= (e.deltaY / g.scale) * speed;
-    });
+    if (e.ctrlKey) {
+      zoom(e, e.deltaY);
+    } else if (e.shiftKey) {
+      updateGlobal((g) => {
+        g.trans_x -= (e.deltaY / g.scale) * speed;
+        g.trans_y -= (e.deltaX / g.scale) * speed;
+      });
+    } else {
+      updateGlobal((g) => {
+        g.trans_x -= (e.deltaX / g.scale) * speed;
+        g.trans_y -= (e.deltaY / g.scale) * speed;
+      });
+    }
   }
 };
