0fbf15b3fbced7b6494b0c2bcc6d800b04384304
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2023-01-26 15:20:29 +0900

    refactor

---
+++ src/Event/handle_making_line.ts
@@ -0,0 +1,48 @@
+import { getGlobal } from "reactn";
+import { TItemId } from "../Global/TItemId";
+import { updateGlobal } from "../Global/updateGlobal";
+import { dev_log } from "../utils/dev";
+import { kozaneba } from "../API/KozanebaAPI";
+import { TAnnotation } from "../Global/TAnnotation";
+import { mark_local_changed } from "../utils/mark_local_changed";
+
+export const handle_making_line = (id: TItemId) => {
+  const g = getGlobal();
+  updateGlobal((g) => {
+    g.mouseState = "";
+  });
+  dev_log("add line", g.line_start, id, g.line_type);
+  if (g.line_start !== "" && g.line_start !== id) {
+    switch (g.line_type) {
+      case "line":
+        kozaneba.add_arrow([g.line_start, id], ["none", "none"]);
+        break;
+      case "arrow":
+        kozaneba.add_arrow([g.line_start, id], ["none", "arrow"]);
+        break;
+      case "double_heads":
+        kozaneba.add_arrow([g.line_start, id], ["arrow", "arrow"]);
+        break;
+      case "double_lines":
+        kozaneba.add_arrow([g.line_start, id], ["none", "none"], true);
+        break;
+      case "delete":
+        const start = g.line_start,
+          end = id;
+        const new_annot = g.annotations.filter((a: TAnnotation) => {
+          if (a.items.length != 2) {
+            return true;
+          }
+          const [v1, v2] = a.items;
+          if ((v1 === start && v2 === end) || (v2 === start && v1 === end)) {
+            return false;
+          }
+          return true;
+        });
+        updateGlobal((g) => {
+          g.annotations = new_annot;
+        });
+        mark_local_changed();
+    }
+  }
+};
--- src/Event/onGenericClick.ts
+++ src/Event/onGenericClick.ts
@@ -6,9 +6,7 @@ import { show_menu } from "../utils/show_menu";
 import { reset_target } from "./fast_drag_manager";
 import { get_item } from "../utils/get_item";
 import { dev_log } from "../utils/dev";
-import { kozaneba } from "../API/KozanebaAPI";
-import { TAnnotation } from "../Global/TAnnotation";
-import { mark_local_changed } from "../utils/mark_local_changed";
+import { handle_making_line } from "./handle_making_line";
 
 export const onGenericClick = (
   event: React.MouseEvent<HTMLDivElement>,
@@ -19,46 +17,8 @@ export const onGenericClick = (
 
   dev_log("onGenelicClick", `g.mouseState=${g.mouseState}`);
 
-  //
-  // add line
   if (g.mouseState === "making_line") {
-    updateGlobal((g) => {
-      g.mouseState = "";
-    });
-    dev_log("add line", g.line_start, id, g.line_type);
-    if (g.line_start !== "" && g.line_start !== id) {
-      switch (g.line_type) {
-        case "line":
-          kozaneba.add_arrow([g.line_start, id], ["none", "none"]);
-          break;
-        case "arrow":
-          kozaneba.add_arrow([g.line_start, id], ["none", "arrow"]);
-          break;
-        case "double_heads":
-          kozaneba.add_arrow([g.line_start, id], ["arrow", "arrow"]);
-          break;
-        case "double_lines":
-          kozaneba.add_arrow([g.line_start, id], ["none", "none"], true);
-          break;
-        case "delete":
-          const start = g.line_start,
-            end = id;
-          const new_annot = g.annotations.filter((a: TAnnotation) => {
-            if (a.items.length != 2) {
-              return true;
-            }
-            const [v1, v2] = a.items;
-            if ((v1 === start && v2 === end) || (v2 === start && v1 === end)) {
-              return false;
-            }
-            return true;
-          });
-          updateGlobal((g) => {
-            g.annotations = new_annot;
-          });
-          mark_local_changed();
-      }
-    }
+    handle_making_line(id);
     return;
   }
 
