95035538ed7fe02d8627b24a4fc8d8dfa4d3356e
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2023-02-27 17:06:27 +0900

    add "merge" menu

--- src/Menu/SelectionMenu.tsx
+++ src/Menu/SelectionMenu.tsx
@@ -1,15 +1,23 @@
 import { Menu, MenuItem } from "@mui/material";
 import React from "react";
 import { getGlobal, useGlobal } from "reactn";
+import { add_kozane } from "../API/add_kozane";
 import { kozaneba } from "../API/KozanebaAPI";
 import { add_bipartite } from "../API/sample/add_bipartite";
 import { UserMenuItem } from "../API/UserMenuItem";
+import { get_middle_point } from "../Canvas/Annotation/get_middle_point";
+import { V2 } from "../dimension/V2";
+import { TWorldCoord } from "../dimension/world_to_screen";
+import { TItemId } from "../Global/TItemId";
+import { updateGlobal } from "../Global/updateGlobal";
 import { reset_selection } from "../Selection/reset_selection";
 import {
   add_arrow_among_selected_items,
   THeadsOption,
 } from "../utils/add_arrow";
+import { create_new_itemid } from "../utils/create_new_itemid";
 import { delete_item_from_world } from "../utils/delete_item_from_world";
+import { get_item } from "../utils/get_item";
 import { make_items_into_new_group } from "../utils/make_items_into_new_group";
 import { mark_local_changed } from "../utils/mark_local_changed";
 import { copy_json } from "./copy_json";
@@ -63,6 +71,74 @@ export const SelectionMenu = () => {
 
   const onAddBipartite = (is_doubled = false, is_arrow = true) => {
     add_bipartite(getGlobal().selected_items, is_doubled, is_arrow);
+    reset_selection();
+    mark_local_changed();
+    setMenu("");
+  };
+
+  const onMerge = () => {
+    const g = getGlobal();
+    const texts = [] as string[];
+    const positions = [] as TWorldCoord[][];
+    const ids = [] as TItemId[];
+    g.selected_items.forEach((id) => {
+      const x = get_item(g, id);
+      if (x.type === "kozane") {
+        if (!texts.includes(x.text)) {
+          texts.push(x.text);
+          positions.push([x.position]);
+        } else {
+          const i = texts.indexOf(x.text);
+          positions[i]!.push(x.position);
+        }
+        ids.push(id);
+      }
+    });
+    if (texts.length === 0) {
+      return; // nothing to merge
+    }
+    let merged_text = "";
+    if (texts.length > 1) {
+      const left_to_right = positions
+        .map(get_middle_point)
+        .map((v, i) => [v, i] as [V2, number])
+        .sort((a, b) => a[0][0] - b[0][0]);
+      merged_text = left_to_right.map((v) => texts[v[1]]).join("/");
+    } else {
+      // length === 1
+      merged_text = texts[0]!;
+    }
+    {
+      const id = create_new_itemid();
+      const position = get_middle_point(
+        positions.flatMap((x) => x)
+      ) as TWorldCoord;
+      add_kozane(merged_text, {
+        position,
+        id,
+      });
+
+      // potential heavy process, O(`num lines` x `num merged items`)
+      const merged_item = id;
+      updateGlobal((g) => {
+        ids.forEach((id) => {
+          g.annotations.forEach((a) => {
+            if (a.type === "line") {
+              if (a.items.includes(id)) {
+                const i = a.items.indexOf(id);
+                a.items[i] = merged_item;
+              }
+            }
+          });
+        });
+      });
+
+      // removed old items
+      ids.forEach((id) => {
+        delete_item_from_world(id);
+      });
+    }
+
     reset_selection();
     mark_local_changed();
     setMenu("");
@@ -194,6 +270,8 @@ export const SelectionMenu = () => {
       <MenuItem onClick={onAddArrowAllHeads} data-testid="copy-text">
         add arrow(head:all)
       </MenuItem> */}
+
+      <MenuItem onClick={onMerge}>merge items</MenuItem>
 
       {kozaneba.user_menus["Selection"]!.map(UserMenuItem)}
 
